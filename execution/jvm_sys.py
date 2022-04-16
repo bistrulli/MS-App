a60872c915450a1f12499883449a50129e11b91f
emilio <emilio@mpbwalt.local>
2022-04-16 11:09:48 +0200

    fixed affinity

--- execution/jvm_sys.py
+++ execution/jvm_sys.py
@@ -363,7 +363,7 @@ if __name__ == "__main__":
         g = None
         jvm_sys = jvm_sys("../", isCpu)
         
-        W=[70,80,90,100,110]
+        W=[1,40,50,60,70,80,90,100,110]
         rtExp=np.zeros([len(W),2])
         rtCI=np.zeros([len(W),2])
         
@@ -387,16 +387,16 @@ if __name__ == "__main__":
                 
                 Client_rt=jvm_sys.batchMeansRT("Client_rtlog.txt",N=30,K=30);
                 if(Client_rt):
-                    e=(Client_rt[1][1]-Client_rt[0])/Client_rt[0]
-                    if(e>0.01):
+                    e=(Client_rt[1][1]-Client_rt[0])
+                    if(e>10**-3):
                         isConverged=isConverged and False
                 else:
                     isConverged=isConverged and False
                         
                 T1_rt=jvm_sys.batchMeansRT("t1_rtlog.txt",N=30,K=30);
                 if(T1_rt):
-                    e=(T1_rt[1][1]-T1_rt[0])/T1_rt[0]
-                    if(e>0.01):
+                    e=(T1_rt[1][1]-T1_rt[0])
+                    if(e>10**-3):
                         isConverged=isConverged and False
                 else:
                     isConverged=isConverged and False
