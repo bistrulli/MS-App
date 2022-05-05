1931e429e04443454f32c689114d4a52e24a9a9c
emilio <emilio@mpbwalt.home>
2022-05-05 14:24:45 +0200

    new data

--- execution/jvm_sys.py
+++ execution/jvm_sys.py
@@ -488,7 +488,7 @@ if __name__ == "__main__":
         
         # W=[4,8,12,16,20,24,28,32,36,40,44,48,52,56,60]
         #W=np.random.randint(low=4,high=200,size=[20]) 
-        W=[2,3,4,5,6,7,8,9,10,11,12,13,20,25,30,25,30,35,40,45,50,60,65,70,80]
+        W=[81]
         rtExp=np.zeros([len(W),3])
         tExp=np.zeros([len(W),3])
         rtCI=np.zeros([len(W),3])
@@ -499,7 +499,7 @@ if __name__ == "__main__":
             
             #NC.append([np.inf,np.random.randint(low=1,high=13),np.random.randint(low=1,high=13)])
             
-            NC.append([np.inf,5,10])
+            NC.append([np.inf,1,10])
             
             sys = jvm_sys("../", isCpu)
             
