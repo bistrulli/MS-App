d0dcb87b520d3a6adafa847de557eb21572579cb
emilio <emilio@mpbwalt.local>
2022-05-11 20:52:50 +0200

    global fit

--- fitLQN/fitLQNLight.jl
+++ fitLQN/fitLQNLight.jl
@@ -1,4 +1,4 @@
-using Printf,Ipopt,MadNLP,Plots,MadNLPMumps,JuMP,MAT,ProgressBars,ParameterJuMP,Statistics
+using AmplNLWriter,Couenne_jll,Printf,Ipopt,MadNLP,Plots,MadNLPMumps,JuMP,MAT,ProgressBars,ParameterJuMP,Statistics
 
 DATA = matread("../execution/data/3tier_all2.mat")
 
@@ -19,8 +19,9 @@ for i=1:sum(nzIdz)
 end
 
 
-model = Model(()->MadNLP.Optimizer(linear_solver=MadNLPLapackCPU,max_iter=100000))
+#model = Model(()->MadNLP.Optimizer(linear_solver=MadNLPLapackCPU,max_iter=100000))
 #model = Model(Ipopt.Optimizer)
+model = Model(() -> AmplNLWriter.Optimizer(Couenne_jll.amplexe))
 # set_optimizer_attribute(model, "linear_solver", "pardiso")
 #set_optimizer_attribute(model, "max_iter", 20000)
 # set_optimizer_attribute(model, "derivative_test", "first-order")
@@ -86,20 +87,20 @@ Xu=RTm.*Tm;
 
 for p=1:npoints
 
-#exp1=@NLexpression(model,(min_((X[2,p])-NC[p,2])+NC[p,2]))
-#exp2=@NLexpression(model,(min_((X[3,p])-NC[p,3])+NC[p,3]))
+exp1=@NLexpression(model,(-(-(X[2,p]-NC[p,2])+((-(X[2,p]-NC[p,2]))^2+10^-10)^(1.0/2))/2.0+NC[p,2]))
+exp2=@NLexpression(model,(-(-(X[3,p]-NC[p,3])+((-(X[3,p]-NC[p,3]))^2+10^-10)^(1.0/2))/2.0+NC[p,3]))
 
 @NLconstraint(model,T[1,p]==P[1,2]*MU[1]*X[1,p])
 @NLconstraint(model,T[2,p]==P[1,3]*MU[1]*X[1,p])
 @NLconstraint(model,T[3,p]==P[1,1]*MU[1]*X[1,p])
 
-@NLconstraint(model,T[4,p]==P[2,1]*MU[2]*(min_((X[2,p])-NC[p,2])+NC[p,2]))
-@NLconstraint(model,T[5,p]==P[2,3]*MU[2]*(min_((X[2,p])-NC[p,2])+NC[p,2]))
-@NLconstraint(model,T[6,p]==P[2,2]*MU[2]*(min_((X[2,p])-NC[p,2])+NC[p,2]))
+@NLconstraint(model,T[4,p]==P[2,1]*MU[2]*exp1)
+@NLconstraint(model,T[5,p]==P[2,3]*MU[2]*exp1)
+@NLconstraint(model,T[6,p]==P[2,2]*MU[2]*exp1)
 
-@NLconstraint(model,T[7,p]==P[3,1]*MU[3]*(min_((X[3,p])-NC[p,3])+NC[p,3]))
-@NLconstraint(model,T[8,p]==P[3,2]*MU[3]*(min_((X[3,p])-NC[p,3])+NC[p,3]))
-@NLconstraint(model,T[9,p]==P[3,3]*MU[3]*(min_((X[3,p])-NC[p,3])+NC[p,3]))
+@NLconstraint(model,T[7,p]==P[3,1]*MU[3]*exp2)
+@NLconstraint(model,T[8,p]==P[3,2]*MU[3]*exp2)
+@NLconstraint(model,T[9,p]==P[3,3]*MU[3]*exp2)
 
 # @constraint(model,[k=1:size(E_abs2,1)],E_abs2[k,p]>=(jump'*T[:,p])[k])
 # @constraint(model,[k=1:size(E_abs2,1)],E_abs2[k,p]>=-(jump'*T[:,p])[k])
@@ -115,33 +116,35 @@ obj=[]
 for p=1:npoints
         @constraint(model,sum(X[:,p])==Cli[p])
         @constraints(model,begin
-                E_abs[1,p]>=(Tm[p,1]-sum(T[[1,2,3],p]))
-                E_abs[1,p]>=-(Tm[p,1]-sum(T[[1,2,3],p]))
+                E_abs[1,p]>=(Tm[p,1]-sum(T[[1,2,3],p]))/Tm[p,1]
+                E_abs[1,p]>=-(Tm[p,1]-sum(T[[1,2,3],p]))/Tm[p,1]
 
-                E_abs[2,p]>=(Tm[p,2]-sum(T[[4,5,6],p]))
-                E_abs[2,p]>=-(Tm[p,2]-sum(T[[4,5,6],p]))
+                E_abs[2,p]>=(Tm[p,2]-sum(T[[4,5,6],p]))/Tm[p,2]
+                E_abs[2,p]>=-(Tm[p,2]-sum(T[[4,5,6],p]))/Tm[p,2]
 
-                E_abs[3,p]>=(Tm[p,3]-sum(T[[7,8,9],p]))
-                E_abs[3,p]>=-(Tm[p,3]-sum(T[[7,8,9],p]))
-
+                E_abs[3,p]>=(Tm[p,3]-sum(T[[7,8,9],p]))/Tm[p,3]
+                E_abs[3,p]>=-(Tm[p,3]-sum(T[[7,8,9],p]))/Tm[p,3]
         end)
 
         for i=1:size(jump,2)
             @NLconstraint(model,RTlqn[i,p]==sum(P[i,j]*P2[i,j]*RTlqn[j,p] for j=1:size(jump,2))+RTs[i,p])
             #@constraint(model,RTlqn[i,p]==sum(P2[i,j]*RTm[p,j] for j=1:size(jump,2))+RTs[i,p])
-            @constraint(model,ERT_abs[i,p]>=(RTlqn[i,p]-RTm[p,i]))
-            @constraint(model,ERT_abs[i,p]>=(-RTlqn[i,p]+RTm[p,i]))
+            @constraint(model,ERT_abs[i,p]>=(RTlqn[i,p]-RTm[p,i])/RTm[p,i])
+            @constraint(model,ERT_abs[i,p]>=(-RTlqn[i,p]+RTm[p,i])/RTm[p,i])
         end
 end
 
 
 #@objective(model,Min, sum(E_abs2[i,p] for i=1:size(E_abs2,1) for p=1:size(E_abs2,2))+sum(E_abs[i,p] for i=1:size(E_abs,1) for p=1:size(E_abs,2))+sum(ERT_abs[i,p] for i=1:size(ERT_abs,1) for p=1:size(E_abs,2)))
-@objective(model,Min, sum(E_abs[i,p] for i=1:size(E_abs,1) for p=1:size(E_abs,2))+sum(ERT_abs[i,p] for i=1:size(ERT_abs,1) for p=1:size(E_abs,2)))
+@objective(model,Min, sum(MU)+sum(E_abs[i,p] for i=1:size(E_abs,1) for p=1:size(E_abs,2))+sum(ERT_abs[i,p] for i=1:size(ERT_abs,1) for p=1:size(E_abs,2)))
 #@objective(model,Min, ETmax+ERTmax)
 JuMP.optimize!(model)
 
 matwrite("fromJulia.mat", Dict(
                "RTlqn" => value.(RTlqn),
                "T" => value.(T),
+               "MU" => value.(MU),
+               "P" => value.(P),
+               "P2" => value.(P.*P2),
                "NCopt" => value.(NC)
        );)
--- fitLQN/plotRes.m
+++ fitLQN/plotRes.m
@@ -1,41 +1,42 @@
 % Idx=NC(:,2)==4;
-% 
+%
 % figure
 % hold on
 % stem(sort(RTm(Idx,2)))
 % stem(sort(RTl(Idx,2)))
 % legend(["RTClient_m","RTClient_p"])
-% 
+%
 % figure
 % hold on
 % stem(sort(Tm(Idx,2)))
 % stem(sort(Tl(Idx,2)))
 % legend(["TClient_m","TClient_p"])
 
-load("/Users/emilio/git/MS-App/execution/data/3tier_learn5.mat");
+load("/Users/emilio/git/MS-App/execution/data/3tier_learn7.mat");
 load("./fromJulia.mat")
 
 CIdx=sum(sum(RTm,2)~=0);
 %CIdx=15;
-% 
+%
 % P=[0.00217905  0.997821
 %  0.996344    0.00365643];
 % P2=[0.0         0.99774
 %  0.00253123  0.0];
 % MU=[3.2865425244638855,9.781509324351875];
 
-P=[ 0.0411824   0.958791    2.64373e-5
- 2.12782e-8  9.64364e-9  1.0
- 0.958716    0.0412837   7.76466e-9];
-
-P2=[    -4.11822e-10   0.957411      2.64366e-5
-  1.98564e-8   -9.64357e-17   1.0
-  0.0292625     2.04815e-8   -7.7646e-17
-];
-
-MU=[    2.6886090256059068
- 9.496661662406366
- 6.6521236275757305];
+% P=[   0.337294  0.33725   0.325456
+%  0.337293  0.33725   0.325458
+%  0.337157  0.337116  0.325727
+%     ];
+% 
+% P2=[   0.0        0.942952  0.0401206
+%  0.0257452  0.0       1.0
+%  0.0        0.012123  0.0
+%     ];
+% 
+% MU=[2.8310356130274985
+%  9.953156595757855
+%  6.730606274746325];
 
 
 RTl=zeros(CIdx,size(RTm,2));
@@ -44,29 +45,31 @@ for i=1:CIdx
     %Kpi=lineQN([Cli(i)],[0,1;1,0],1./[0.9977,0.10316562661911506],[inf,5]);
     Kpi=lineQN([Cli(i)],P,MU,NC(i,:));
     
-%   RTl(i,:)=Kpi(2,:);
-%     RTl(i,1)=sum(RTl(i,:));
+    %   RTl(i,:)=Kpi(2,:);
+    %     RTl(i,1)=sum(RTl(i,:));
     RTl(i,:)=solveRT(P2,Kpi(2,:));
     
     Tl(i,:)=Kpi(4,:);
+end
+
+for cmp=1:size(RTm,2)
+    figure
+    hold on
+    box on
+    grid on
+    stem(RTm(1:CIdx,cmp),"linewidth",1.1,'LineStyle','none')
+    stem(RTl(:,cmp),"-.","linewidth",1.3,'LineStyle','none')
+    %plot(Cli(1:CIdx),RTlqn(:,1:CIdx)',"--","linewidth",1.3)
+    legend(["RT_m","RT_p"])
 end
 
 figure
 hold on
 box on
 grid on
-plot(Cli(1:CIdx),RTm(1:CIdx,:),"linewidth",1.1)
-plot(Cli(1:CIdx),RTl(:,:),"-.","linewidth",1.3)
-% plot(Cli(1:CIdx),RTlqn(:,1:CIdx)',"--","linewidth",1.3)
-legend(["RT_m","RT_p"])
-
-figure
-hold on
-box on
-grid on
-plot(Cli(1:CIdx),Tm(1:CIdx,:),"linewidth",1.1)
-plot(Cli(1:CIdx),Tl(:,:),"-.","linewidth",1.3)
-% plot(Cli(1:CIdx),sum(T([1,2,3],1:CIdx)),"--","linewidth",1.3)
+plot(Tm(1:CIdx,:),"linewidth",1.1)
+plot(Tl(:,:),"-.","linewidth",1.3)
+%plot(Cli(1:CIdx),sum(T([1,2,3],1:CIdx)),"--","linewidth",1.3)
 legend(["T_m","T_p"])
 
 figure
