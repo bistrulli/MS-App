4148c3cdac35a1273c4305e21b04214cd8f1d20d
emilio <emilio@mpbwalt.local>
2022-05-12 21:20:25 +0200

    wi data

--- execution/jvm_sys.py
+++ execution/jvm_sys.py
@@ -499,7 +499,7 @@ if __name__ == "__main__":
             
             #NC.append([np.inf,np.random.randint(low=1,high=13),np.random.randint(low=1,high=13)])
             
-            NC.append([np.inf,13,15])
+            NC.append([np.inf,20,25])
             
             sys = jvm_sys("../", isCpu)
             
