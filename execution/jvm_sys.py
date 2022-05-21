4356424276a6f6755358224dd9c10cfffba59416
emilio <emilio@mpbwalt.local>
2022-05-21 17:46:11 +0200

    asynch data

--- execution/jvm_sys.py
+++ execution/jvm_sys.py
@@ -540,7 +540,7 @@ if __name__ == "__main__":
             sys.stopClient()
             sys.stopSystem()
             
-            savemat("./data/3tier_learnHDVCWI2.mat", {"RTm":rtExp,"rtCI":rtCI,"Tm":tExp,"tCI":tCI,"Cli":W,"NC":NC})
+            savemat("./data/3tier_learnAsynch.mat", {"RTm":rtExp,"rtCI":rtCI,"Tm":tExp,"tCI":tCI,"Cli":W,"NC":NC})
         
         
             
