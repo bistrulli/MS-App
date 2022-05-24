7beffc6f32b65ffd6991f395aa5a6dbe16d2dc34
emilio <emilio@mpbwalt.local>
2022-05-24 15:24:03 +0200

    minor

--- MS-Client/src/main/java/app/Main.java
+++ MS-Client/src/main/java/app/Main.java
@@ -89,7 +89,7 @@ public class Main {
 		clientEntries.put("think", Client.class);
 		clientEntries_stimes.put("think", 300l);
 		final SimpleTask client = new SimpleTask(clientEntries, clientEntries_stimes, Main.initPop, "Client",
-				Main.jedisHost, null,500l);
+				Main.jedisHost, null,100l);
 		Client.setTier1Host(Main.tier1Host);
 		return new SimpleTask[] { client };
 	}
